325192d5f68f14225683155f632f243cd8be8ba3
hafus <konsantin.pirs@gmai.com>
2022-12-27 18:20:25 +0300

    version 1.0

---
+++ script.js
@@ -0,0 +1,36 @@
+function clockInJS() {
+    // hook div with id "clock" 
+    let clock = document.querySelector("#clock");
+    
+    // init object with date and time
+    let time = new Date;
+
+    // init vars of hour and so on
+    let h = time.getHours();
+    let m = time.getMinutes();
+    let s = time.getSeconds();
+    let am_pm = "AM"
+
+    // logic for change AM to PM
+    if (h > 12) {
+        h -= 12;
+        am_pm = "PM"
+    } if (h == 0) {
+        h = 12;
+        am_pm = "AM"
+    }
+
+    // add zero befor number if {variable} < 12
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    // formatting text
+    let text = `${h}:${m}:${s} ${am_pm}`;
+    
+    // ouput time to div
+    clock.innerHTML = text
+};
+
+// repeat function 1 time per second
+setInterval(clockInJS, 1000)
